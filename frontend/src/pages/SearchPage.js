685567d330a1b961e781fb1c4c9f25445f8ea9cb
williams <oneal.wills@gmail.com>
2022-12-15 10:47:57 +0100

    css complete

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -51,107 +51,112 @@ export default function SearchPage(props) {
     return `/search/type/${filterType}/model/${filterModel}/min/${filterMin}/max/${filterMax}/rating/${filterRating}/order/${sortOrder}`;
   };
   return (
-    <div>
-      <div>
-        {loading ? (
-          <LoadingBox></LoadingBox>
-        ) : error ? (
-          <MessageBox type="danger">{error}</MessageBox>
-        ) : (
-          <div>{products.length} Results</div>
-        )}
-      </div>
-      <div>
-        Sort by{" "}
-        <select
-          value={order}
-          onChange={(e) => {
-            props.history.push(getFilterUrl({ order: e.target.value }));
-          }}
-        >
-          <option value="newest">Newest Arrivals</option>
-          <option value="lowest">Price: Low to High</option>
-          <option value="highest">Price: High to Low</option>
-          <option value="toprated">Avg. Customer Reviews</option>
-        </select>
-      </div>
-      <div>
-        {loading ? (
-          <LoadingBox></LoadingBox>
-        ) : error ? (
-          <MessageBox type="danger">{error}</MessageBox>
-        ) : (
-          <>
-            {products.length === 0 && <MessageBox>No product Found</MessageBox>}
-            <div>
-              {products.map((product) => (
-                <Product key={product._id} product={product}></Product>
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-      <div>
-        {loadingTypes ? (
-          <LoadingBox></LoadingBox>
-        ) : errorTypes ? (
-          <MessageBox variant="danger">{errorTypes}</MessageBox>
-        ) : (
-          <ul>
-            <li>
-              <Link
-                className={"all" === type ? "active" : ""}
-                to={getFilterUrl({ type: "all" })}
-              >
-                Any
-              </Link>
-            </li>
-            {types.map((t) => (
-              <li key={t}>
+    <div className="searchpage">
+      <div className="margin-t">
+        <div className="searchpage__messagebox">
+          {loading ? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+            <MessageBox type="danger">{error}</MessageBox>
+          ) : (
+            <div>{products.length} Results</div>
+          )}
+        </div>
+        <div className="searchpage__filter">
+          Sort by{" "}
+          <select
+            value={order}
+            onChange={(e) => {
+              props.history.push(getFilterUrl({ order: e.target.value }));
+            }}
+          >
+            <option value="newest">Newest Arrivals</option>
+            <option value="lowest">Price: Low to High</option>
+            <option value="highest">Price: High to Low</option>
+            <option value="toprated">Avg. Customer Reviews</option>
+          </select>
+        </div>
+        <div className="searchpage__types">
+          {loadingTypes ? (
+            <LoadingBox></LoadingBox>
+          ) : errorTypes ? (
+            <MessageBox variant="danger">{errorTypes}</MessageBox>
+          ) : (
+            <ul>
+              <li>
                 <Link
-                  className={t === type ? "active" : ""}
-                  to={getFilterUrl({ type: t })}
+                  className={"all" === type ? "active" : ""}
+                  to={getFilterUrl({ type: "all" })}
                 >
-                  {t}
+                  Any
+                </Link>
+              </li>
+              {types.map((t) => (
+                <li key={t}>
+                  <Link
+                    className={t === type ? "active" : ""}
+                    to={getFilterUrl({ type: t })}
+                  >
+                    {t}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="searchpage__price">
+          <h3>Price</h3>
+          <ul>
+            {prices.map((p) => (
+              <div key={p.name}>
+                <Link
+                  to={getFilterUrl({ min: p.min, max: p.max })}
+                  className={
+                    `${p.min}-${p.max}` === `${min}-${max}` ? "active" : ""
+                  }
+                >
+                  {p.name}
+                </Link>
+              </div>
+            ))}
+          </ul>
+        </div>
+        <div className="searchpage__customer__review">
+          <h3>Avg. Customer Review</h3>
+          <ul>
+            {ratings.map((r) => (
+              <li key={r.name}>
+                <Link
+                  to={getFilterUrl({ rating: r.rating })}
+                  className={`${r.rating}` === `${rating}` ? "active" : ""}
+                >
+                  <Rating caption={" & up"} rating={r.rating}></Rating>
                 </Link>
               </li>
             ))}
           </ul>
-        )}
+        </div>
       </div>
-      <div>
-        <h3>Price</h3>
-        <ul>
-          {prices.map((p) => (
-            <div key={p.name}>
-              <Link
-                to={getFilterUrl({ min: p.min, max: p.max })}
-                className={
-                  `${p.min}-${p.max}` === `${min}-${max}` ? "active" : ""
-                }
-              >
-                {p.name}
-              </Link>
-            </div>
-          ))}
-        </ul>
+      <div className="margin-t">
+        <div className="searchpage__products">
+          {loading ? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+            <MessageBox type="danger">{error}</MessageBox>
+          ) : (
+            <>
+              {products.length === 0 && (
+                <MessageBox>No product Found</MessageBox>
+              )}
+              <div>
+                {products.map((product) => (
+                  <Product key={product._id} product={product}></Product>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div>
-        <h3>Avg. Customer Review</h3>
-        <ul>
-          {ratings.map((r) => (
-            <li key={r.name}>
-              <Link
-                to={getFilterUrl({ rating: r.rating })}
-                className={`${r.rating}` === `${rating}` ? "active" : ""}
-              >
-                <Rating caption={" & up"} rating={r.rating}></Rating>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div></div>
     </div>
   );
 }
